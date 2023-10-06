dcba1c4098ee5db0dfd736497c15886aa660cc10
Denis H. Roldán <112777347+denisrold@users.noreply.github.com>
2023-10-06 01:01:26 -0300

    realizado, faltan estilos

--- api/index.js
+++ api/index.js
@@ -5,5 +5,18 @@ import { Server as SocketServer } from "socket.io";
 const app = express();
 const server = http.createServer(app);
 const io = new SocketServer(server);
+//ver archivo vite.config por tema de cors.
 
-server.listen(3000, console.log("listen port 3000"));
+io.on("connection", (socket) => {
+  console.log(socket.id);
+  //Escuchando un evento desde el front
+  socket.on("message", (body) => {
+    //envio a todos los clientes un evento
+    socket.broadcast.emit("message", {
+      body,
+      from: socket.id.slice(6),
+    });
+  });
+});
+
+server.listen(4000, console.log("listen port 4000"));
