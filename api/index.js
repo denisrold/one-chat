54164e753c38f58641900c0bbd0bf5cdb0c28389
Denis H. Roldán <112777347+denisrold@users.noreply.github.com>
2023-10-05 21:48:04 -0300

    server and api

--- api/index.js
+++ api/index.js
@@ -1,4 +1,9 @@
 import express from "express";
+import http from "http";
+import { Server as SocketServer } from "socket.io";
 
 const app = express();
-app.listen(3000, console.log("listen port 3000"));
+const server = http.createServer(app);
+const io = new SocketServer(server);
+
+server.listen(3000, console.log("listen port 3000"));
